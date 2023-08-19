e54fff08c3912334c2c785130ace04c43b7bc560
Andres Romero <andresfelipe328@hotmail.com>
2023-08-18 18:27:53 -0700

    initial commit

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,16 +1,11 @@
-import { Metadata } from "next";
+import React from "react";
 import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
-import { authOptions } from "./api/auth/[...nextauth]/route";
 
 import BasicAnimLayout from "@/components/layouts/BasicAnimLayout";
 
-export const metadata: Metadata = {
-  title: "Cuispiria - Home",
-  description: "Recepies & Recepie Management App",
-};
-
-export default async function Home() {
+const page = async () => {
   // Variables
   const session = await getServerSession(authOptions);
   if (!session) redirect("/search-recipe");
@@ -18,8 +13,10 @@ export default async function Home() {
   return (
     <BasicAnimLayout>
       <div className="max-w-7xl mx-auto w-full flex-1 flex flex-col gap-2 p-4">
-        <h2>Dashboard</h2>
+        <h2>Settings</h2>
       </div>
     </BasicAnimLayout>
   );
-}
+};
+
+export default page;
